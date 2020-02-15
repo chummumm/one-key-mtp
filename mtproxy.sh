4f44af050488bfb1ae570ce8d03a6739bf3bbcdc
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-15 16:14:19 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -20,5 +20,5 @@ systemctl start mtproxy &&
 echo 完成. &&
 echo mtproxy.service已注册,通过systemctl status mtproxy可查看配置信息(如果有防火墙请手动放行端口). &&
 echo 当手机用户使用systemctl status mtproxy查看配置信息显示不全时请使用方向键右键进行查看. &&
-echo '删除mtproxy及其守护进程请运行： https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh' &&
+echo '删除mtproxy及其守护进程请运行：wget https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh' &&
 rm -- "$0"
