dffaa3794f9537bf3248d41cb7970be5dc2c543a
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-16 09:24:44 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -21,6 +21,7 @@ sed -i "s/mtprotoproxy.py/\/etc\/mtproxy\/mtprotoproxy.py/g" /etc/systemd/system
 systemctl daemon-reload && 
 systemctl enable mtproxy &&
 systemctl start mtproxy &&
+systemctl restart mtproxy &&
 echo 完成. &&
 echo 'mtproxy.service已注册,通过systemctl status mtproxy可查看配置信息(如果有防火墙请手动放行端口).' &&
 echo -e '\033[32m注意！！！！！！使用systemctl status mtproxy查看配置信息显示不全时请使用方向键右键进行查看！！！！！！\033[0m' &&
