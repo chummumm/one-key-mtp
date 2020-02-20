c9c07a7f94b32b1d787ad71a6e5ecad30c852b70
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-21 00:35:57 +0800

    Update deletemtproxy.sh

--- deletemtproxy.sh
+++ deletemtproxy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo 开始删除mtproxy...... &&
-systemctl stop mtproxy &&
-systemctl disable mtproxy &&
-systemctl daemon-reload &&
-rm -rf /etc/mtproxy &&
-rm /etc/systemd/system/mtproxy.service &&
-echo mtproxy删除完成. &&
+echo 开始删除mtproxy......
+systemctl stop mtproxy
+systemctl disable mtproxy
+systemctl daemon-reload
+rm -rf /etc/mtproxy
+rm /etc/systemd/system/mtproxy.service
+echo mtproxy删除完成.
 rm -- "$0"
