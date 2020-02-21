4adfc7cfc3c338a03fd1662f64e1ac15b9d73482
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-21 12:36:50 +0800

    Update mtproxy-daemon.sh

--- mtproxy-daemon.sh
+++ mtproxy-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 开始注册mtproxy守护进程......
+echo "开始注册mtproxy守护进程......"
 wget -q --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/mtproxy.service -O /etc/systemd/system/mtproxy.service
 location=$(find / -name mtprotoproxy.py)
 sed -i "s#mtprotoproxy.py#$location#g" /etc/systemd/system/mtproxy.service
@@ -7,8 +7,8 @@ systemctl daemon-reload
 systemctl enable mtproxy
 systemctl start mtproxy
 systemctl restart mtproxy
-echo 完成.
-echo 'mtproxy.service已注册,通过systemctl status mtproxy可查看配置信息(如果有防火墙请手动放行端口).'
-echo -e '\033[32m注意！！！！！！使用systemctl status mtproxy查看配置信息显示不全时请使用方向键右键进行查看！！！！！！\033[0m'
-echo '删除mtproxy守护进程请运行： wget https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy-daemon.sh && bash deletemtproxy-daemon.sh'
+echo "完成."
+echo "mtproxy.service已注册,通过systemctl status mtproxy可查看配置信息(如果有防火墙请手动放行端口)."
+echo -e "\033[32m注意！！！！！！使用systemctl status mtproxy查看配置信息显示不全时请使用方向键右键进行查看！！！！！！\033[0m"
+echo "删除mtproxy守护进程请运行： wget https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy-daemon.sh && bash deletemtproxy-daemon.sh"
 rm -- "$0"
