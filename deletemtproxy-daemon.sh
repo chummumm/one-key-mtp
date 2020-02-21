d728690afaea267f43613af57c33084bdb522fc9
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-21 08:58:26 +0800

    Update deletemtproxy-daemon.sh

--- deletemtproxy-daemon.sh
+++ deletemtproxy-daemon.sh
@@ -2,7 +2,7 @@
 echo 开始删除mtproxy守护进程......
 systemctl stop mtproxy
 systemctl disable mtproxy
-systemctl daemon-reload &&
+systemctl daemon-reload
 rm /etc/systemd/system/mtproxy.service
 echo mtproxy守护进程删除完成.
 rm -- "$0"
