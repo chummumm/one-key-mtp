b70ea3ee266aae5fb936fec304ac422bda18ed69
chummumm <50511147+chummumm@users.noreply.github.com>
2020-02-21 12:34:16 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 开始安装mtproxy
+echo "开始安装mtproxy"
 apt update 2>/dev/null
 apt install git python3-pip curl xxd bc -y 2>/dev/null
 yum update -y 2>/dev/null
@@ -9,10 +9,10 @@ if [ -d "/etc/mtproxy" ]; then
   rm -rf /etc/mtproxy
 fi
 git clone https://github.com/chummumm/mtprotoproxy.git /etc/mtproxy
-echo -n '请输入mtproxy运行端口:'
+echo -n "请输入mtproxy运行端口:"
 read num
 if [ ! -n "$num" ]; then
-  echo -e '\033[32m端口已设置为默认（1973）\033[0m'
+  echo -e "\033[32m端口已设置为默认（1973）\033[0m"
   num=1973
 else
   judge=`echo "$num*1" | bc `
@@ -20,15 +20,15 @@ else
     sed -i "s/1973/$num/g" /etc/mtproxy/config.py 
   fi
 fi
-echo 正在随机生成secret......
+echo "正在随机生成secret......"
 secret=$(head -c 16 /dev/urandom | xxd -ps)
 sed -i "s/0000000054655212aa12221200000001/$secret/g" /etc/mtproxy/config.py
 sed -i 's/"secure": False,/"secure": True,/g' /etc/mtproxy/config.py
 sed -i 's/AD_TAG/#AD_TAG/g' /etc/mtproxy/config.py
-echo -n '请输入需要伪装的域名:'
+echo -n "请输入需要伪装的域名:"
 read domain
 if [ ! -n "$domain" ]; then
-  echo -e '\033[32m使用默认伪装域名（www.cloudflare.com）\033[0m'
+  echo -e "\033[32m使用默认伪装域名（www.cloudflare.com）\033[0m"
   domain=www.cloudflare.com
 else
   sed -i "s/www.cloudflare.com/$domain/g" /etc/mtproxy/config.py 
@@ -37,23 +37,23 @@ STR="$domain"
 HEXVAL=$(xxd -pu <<< "$STR")
 hexdomain=${HEXVAL%0a}
 ip=$(curl -4 -k ip.sb)
-echo 开始注册mtproxy守护进程......
+echo "开始注册mtproxy守护进程......"
 wget -q --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/mtproxy.service -O /etc/systemd/system/mtproxy.service
 sed -i "s/mtprotoproxy.py/\/etc\/mtproxy\/mtprotoproxy.py/g" /etc/systemd/system/mtproxy.service
 systemctl daemon-reload
 systemctl enable mtproxy
 systemctl start mtproxy
 systemctl restart mtproxy
-echo 完成.
+echo "完成."
 clear
-echo 'mtproxy.service已注册
-echo -e '\033[34m请手动放行防火墙端口.\033[0m'
-echo -e '\033[32m请使用 systemctl status mtproxy 命令查看证书是否获取成功！！！！！！\033[0m'
-echo -e '\033[32m代理信息：\033[0m'
+echo "mtproxy.service已注册"
+echo -e "\033[34m请手动放行防火墙端口.\033[0m"
+echo -e "\033[32m请使用 systemctl status mtproxy 命令查看证书是否获取成功！！！！！！\033[0m"
+echo -e "\033[32m代理信息：\033[0m"
 echo -e "\033[32mtg://proxy?server=$ip&port=$num&secret=dd$secret\033[0m"
 echo -e "\033[32mtg://proxy?server=$ip&port=$num&secret=ee$secret$hexdomain\033[0m"
 echo -e "\033[32mtg://proxy?server=$ip&port=$num&secret=dd$secret\033[0m" > /etc/mtproxy/secret
 echo -e "\033[32mtg://proxy?server=$ip&port=$num&secret=ee$secret$hexdomain\033[0m" >> /etc/mtproxy/secret
-echo '后续查看配置信息可使用cat /etc/mtproxy/secret'
-echo '删除mtproxy及其守护进程请运行： wget --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh'
+echo "后续查看配置信息可使用cat /etc/mtproxy/secret"
+echo "删除mtproxy及其守护进程请运行： wget --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh"
 rm -- "$0"
