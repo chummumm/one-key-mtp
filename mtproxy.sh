5af8a325d627b00e15c66fb21a413cb67a0e6445
chummumm <50511147+chummumm@users.noreply.github.com>
2020-03-18 23:21:25 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -- "$0"
 echo "开始安装mtproxy"
 apt update 2>/dev/null
 apt install git python3-pip curl xxd bc lsof -y 2>/dev/null
@@ -8,30 +9,31 @@ pip3 install cryptography
 if [ -d "/etc/mtproxy" ]; then
   rm -rf /etc/mtproxy
 fi
+success=0
 git clone https://github.com/chummumm/mtprotoproxy.git /etc/mtproxy
-echo -n -e "\033[32m请输入mtproxy运行端口:\033[0m"
-read num
-if [ ! -n "$num" ]; then
-  echo -e "\033[32m端口已设置为默认（1973）\033[0m"
-  num=1973
-else
-  judge=`echo "$num*1" | bc `
-  if [ $judge -ne 0 2>/dev/null ]; then
-    echo "正在判断端口是否被占用......"
-    port_test=`lsof -i:$num | grep -v "PID" | awk '{print $2}'`
-    if [ "$port_test" != "" ]; then
-      echo -e "\033[31m端口已被占用\033[0m"
-      rm -- "$0"
-      exit 0
-    else
-      sed -i "s/1973/$num/g" /etc/mtproxy/config.py
-    fi
+until [ $success = 1 ]
+do
+  echo -n -e "\033[32m请输入mtproxy运行端口:\033[0m"
+  read num
+  if [ ! -n "$num" ]; then
+    echo -e "\033[32m端口已设置为默认（1973）\033[0m"
+    num=1973
   else
-    echo -e "\033[31m输入错误，端口号应为整数\033[0m"
-    rm -- "$0"
-    exit 0
+    judge=`echo "$num*1" | bc `
+    if [ $judge -ne 0 2>/dev/null ]; then
+      echo "正在判断端口是否被占用......"
+      port_test=`lsof -i:$num | grep -v "PID" | awk '{print $2}'`
+      if [ "$port_test" != "" ]; then
+        echo -e "\033[31m端口已被占用\033[0m"
+      else
+        sed -i "s/1973/$num/g" /etc/mtproxy/config.py
+	success=1
+      fi
+    else
+      echo -e "\033[31m输入错误，端口号应为整数\033[0m"
+    fi
   fi
-fi
+done
 echo "正在随机生成secret......"
 secret=$(head -c 16 /dev/urandom | xxd -ps)
 sed -i "s/0000000054655212aa12221200000001/$secret/g" /etc/mtproxy/config.py
@@ -68,4 +70,3 @@ echo -e "\033[36mtg://proxy?server=$ip&port=$num&secret=dd$secret\033[0m" > /etc
 echo -e "\033[36mtg://proxy?server=$ip&port=$num&secret=ee$secret$hexdomain\033[0m" >> /etc/mtproxy/secret
 echo -e "\033[36m后续查看配置信息可使用 cat /etc/mtproxy/secret\033[0m"
 echo "删除mtproxy及其守护进程请运行： wget --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh"
-rm -- "$0"
